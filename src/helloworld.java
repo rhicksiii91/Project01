ef08296cd01b5aafe83c95aff3b8b53aaad275d5
Robert Hicks <roberthicksiii@gmail.com>
2018-08-08 11:30:03 -0400

    First test, please work

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by robhicks on 8/8/18.
+ */
+public class helloworld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
